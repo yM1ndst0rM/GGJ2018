36383924deeb526236112752699c536bcec03e7e
Nelson Yiap <nelson.yiap@rewe-digital.com>
2018-01-26 22:02:00 +0100

    add FireBullet script and Bullet prefab

---
+++ Assets/FireBullet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBullet : MonoBehaviour {
+
+	public GameObject bulletPrefab;
+	public Transform bulletSpawn;
+
+	// Use this for initialization
+	void Start () {
+		Fire();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Fire() {
+		var bullet = (GameObject)Instantiate (
+		    bulletPrefab,
+		    bulletSpawn.position,
+		    bulletSpawn.rotation);
+
+	    // Adding velocity
+	    bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6; // todo: link this to the player's rotation
+	}
+
+}
