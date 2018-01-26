def9c642d40b5ada8ac6084923c8571f943655f0
Yuliy Khorolinskyy <yk.email@yahoo.de>
2018-01-26 23:46:31 +0100

    Movement: Changed the movement script to use delayed command interface.

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,34 +11,49 @@ public class Movement : MonoBehaviour {
     private float TRANSLATE_SPEED = 4;
 
     private Vector2 forwardVector;
+    private float inputForward = 0;
+    private float inputRotation = 0;
 
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start () {
         rb = GetComponent<Rigidbody2D>();
 	}
 
     // Update is called once per frame
     void Update() {
 
-        float inputForward = 0;
-        if (Input.GetKey(KeyCode.W)) {
-            inputForward += 1;
-        }
-        if (Input.GetKey(KeyCode.S)) {
-            inputForward -= 1;
-        }
-
-        float inputRotation = 0;
-        if (Input.GetKey(KeyCode.A)) {
-            inputRotation += 1;
-        }
-        if (Input.GetKey(KeyCode.D)) {
-            inputRotation -= 1;
-        }
-
         forwardVector.Set(transform.up.x, transform.up.y);
         rb.velocity = forwardVector * inputForward * TRANSLATE_SPEED;
 
         rb.angularVelocity = inputRotation * ROTATION_SPEED;
+
+        inputRotation = 0;
+        inputForward = 0;
+    }
+
+    public void PerformCommand(DelayedCommand.Command c)
+    {
+        switch (c)
+        {
+            case DelayedCommand.Command.UP:
+                inputForward += 1;
+                break;
+            case DelayedCommand.Command.DOWN:
+                inputForward -= 1;
+
+                break;
+            case DelayedCommand.Command.LEFT:
+                inputRotation += 1;
+                break;
+            case DelayedCommand.Command.RIGHT:
+                inputRotation -= 1;
+                break;
+            case DelayedCommand.Command.ATTACK:
+                //empty
+                break;
+            default:
+                throw new ArgumentOutOfRangeException("c", c, null);
+        }
     }
 }
