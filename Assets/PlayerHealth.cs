f6f1ca18ec4d10114017877031ff3ec7afbe4031
Emil Larsson <emil@flying-sheep.com>
2018-01-27 12:29:29 +0100

    Player life wip

---
+++ Assets/PlayerHealth.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+
+    public bool alive { get; private set; }
+    public int life = 1;
+
+	// Use this for initialization
+	void Start () {
+        alive = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
