078917f7bbfe377f4d59a8a6f6ef35d6439fe433
Nelson Yiap <nelson.yiap@rewe-digital.com>
2018-01-27 21:04:02 +0100

    Connects volume settings

---
+++ Assets/scripts/UpdateMasterVolume.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateMasterVolume : MonoBehaviour {
+
+	private Slider self;
+
+	// Use this for initialization
+	void Start () {
+		self = GetComponent<Slider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		AudioListener.volume = self.value / self.maxValue;
+	}
+}
