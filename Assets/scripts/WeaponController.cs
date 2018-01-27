ac3476b4a02177655df04ae4c21faa90e618b316
Yuliy Khorolinskyy <yk.email@yahoo.de>
2018-01-27 14:15:12 +0100

    WeaponController: Added script which manages the direction of the weapon.

---
+++ Assets/scripts/WeaponController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    private Vector2 AimVector { get; set; }
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    if (AimVector.magnitude > 0)
+	    {
+	        GetComponent<Transform>().up = AimVector.normalized;
+	    }
+	}
+
+    public void Aim(Vector2 aimDirection)
+    {
+        AimVector = aimDirection;
+    }
+}
