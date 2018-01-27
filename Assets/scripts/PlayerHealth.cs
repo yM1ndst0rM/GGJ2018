a9b232918b99c00cd9a629664d47a7af236e4a5f
Emil Larsson <emil@flying-sheep.com>
2018-01-27 12:40:13 +0100

    Player health

--- Assets/scripts/PlayerHealth.cs
+++ Assets/scripts/PlayerHealth.cs
@@ -5,15 +5,32 @@ using UnityEngine;
 public class PlayerHealth : MonoBehaviour {
 
     public bool alive { get; private set; }
-    public int life = 1;
+    public float life = 1;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         alive = true;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
+
+    public void DamagePlayer(float damage)
+    {
+        if (damage < 0 || !alive) return;
+
+        life -= damage;
+
+        if (life < 0)
+        {
+            triggerDeath();
+        }
+    }
+
+    private void triggerDeath()
+    {
+        if (!alive) return;
+
+        Debug.Log("Player Died");
+        alive = false;
+        //todo: detha death anims, sounds etc..    
+    }
 }
