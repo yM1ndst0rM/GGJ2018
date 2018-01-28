0bee8d387e845e610ac4e749d4e94cbadb59e277
Yuliy Khorolinskyy <yk.email@yahoo.de>
2018-01-28 01:15:33 +0100

    Trigger script: Simplet script to connect functionality.

---
+++ Assets/TriggerAction.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerAction : MonoBehaviour
+{
+    public UnityEvent OnTriggered;
+
+    public void TriggerEvent()
+    {
+        OnTriggered.Invoke();
+    }
+}
