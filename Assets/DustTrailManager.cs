4d21b5f286823d5619a1906e72b06b77105af3f6
Yuliy Khorolinskyy <yk.email@yahoo.de>
2018-01-28 01:18:06 +0100

    Dust trail: Added simple control script.:

---
+++ Assets/DustTrailManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DustTrailManager : MonoBehaviour
+{
+
+    public Transform SourceObjectPosition;
+
+    public void OnReleaseDust()
+    {
+        var myTransform = GetComponent<Transform>();
+        myTransform.position = new Vector3(SourceObjectPosition.position.x, SourceObjectPosition.position.y, myTransform.position.z);
+
+        GetComponent<Animator>().SetTrigger("Release Dust");
+    }
+}
