f281949e57d528b4c7b9cecf89706aceef830b0a
Nelson Yiap <nelson.yiap@rewe-digital.com>
2018-01-28 12:28:01 +0100

    Settings Screen Transition

---
+++ Assets/scripts/ScreenTransitions.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScreenTransitions : MonoBehaviour {
+
+	private static string ZOOMED_OUT = "ZOOMED_OUT";
+	private static string ZOOMED_IN = "ZOOMED_IN";
+	private static string ZOOMING_OUT = "ZOOMING_OUT";
+	private static string ZOOMING_IN = "ZOOMING_IN";
+
+	Camera camera;
+	public string state;
+
+	// Use this for initialization
+	void Start () {
+		camera = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (state == ZOOMING_OUT) {
+			camera.orthographicSize += 0.2f;
+			if (camera.orthographicSize > 20) {
+				state = ZOOMED_OUT;
+				SceneManager.LoadScene("StartScene");
+			}
+		}
+		if (state == ZOOMING_IN) {
+			camera.orthographicSize -= 0.1f;
+			if (camera.orthographicSize < 5) {
+				camera.orthographicSize = 5;
+				state = ZOOMED_IN;
+			}
+		}
+	}
+
+	public void ExitSettingsOnClick() {
+		if (state == ZOOMED_IN) {
+			state = ZOOMING_OUT;
+		}
+	}
+
+}
